b092a507081570771fe1590f48dba5b2d89f04c8
arimohy <arimohy@gmail.com>
2021-09-30 06:04:41 -0500

    commit inicial

---
+++ src/main/java/co/com/sofka/DDDBiblioteca/DddBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package co.com.sofka.DDDBiblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddBibliotecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/sofka/DDDBiblioteca/DddBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.DDDBiblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
